08ab22791aa24d75bdeda72b07fdcb8d7f41544d
jorge-armando-navarro-flores <jorge.armando.navarro.flores@gmail.com>
2021-08-04 12:00:29 -0500

    colorgram

--- main.py
+++ main.py
@@ -1,19 +1,18 @@
-# import colorgram
+import colorgram
 from random import choice
 from turtle import Turtle, Screen
-# rgb_colors = []
-#
-# colors = colorgram.extract('image.jpg', 30)
-# for color in colors:
-#     r = color.rgb.r
-#     g = color.rgb.g
-#     b = color.rgb.b
-#     new_color = (r, g, b)
-#     rgb_colors.append(new_color)
-#
-# print(rgb_colors)
+color_list = []
 
-color_list = [(202, 164, 109), (238, 240, 245), (150, 75, 49), (223, 201, 135), (52, 93, 124), (172, 154, 40), (140, 30, 19), (133, 163, 185), (198, 91, 71), (46, 122, 86), (72, 43, 35), (145, 178, 148), (13, 99, 71), (233, 175, 164), (161, 142, 158), (105, 74, 77), (55, 46, 50), (183, 205, 171), (36, 60, 74), (18, 86, 90), (81, 148, 129), (148, 17, 20), (14, 70, 64), (30, 68, 100), (107, 127, 153), (174, 94, 97), (176, 192, 209)]
+colors = colorgram.extract('image.jpg', 30)
+for color in colors:
+    r = color.rgb.r
+    g = color.rgb.g
+    b = color.rgb.b
+    if not (r > 230 and g > 230 and b > 230):
+        new_color = (r, g, b)
+        color_list.append(new_color)
+
+print(color_list)
 
 Screen().colormode(255)
 tim = Turtle()
